ee994db35a35d3ae35a733fbc036c64b28f3c564
Jonah <jghebremichael95@gmail.com>
2023-01-06 15:13:29 -0500

    Done

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Hello, enter in the amount of money you wish to deposit: ");
+            double amount;
+            bool parsed = double.TryParse(Console.ReadLine(), out amount);
+
+            while(!parsed)
+            {
+                Console.Write("Please enter a valid amount: ");
+                parsed = double.TryParse(Console.ReadLine(), out amount);
+            }
+
+            var bankAcc = new BankAccount();
+
+            bankAcc.Deposit(amount);
+
+            Console.WriteLine($"You have {bankAcc.GetBalance()}$ in your account");
+
         }
     }
 }
